b8b030beb0c48296de8c504900239affb04ce98b
start.vaadin.com <no-reply@vaadin.com>
2024-12-19 09:30:16 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/myview/MyViewView.java
@@ -0,0 +1,56 @@
+package com.example.application.views.myview;
+
+import com.example.application.data.SamplePerson;
+import com.example.application.services.SamplePersonService;
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.dependency.Uses;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.spring.data.VaadinSpringDataHelpers;
+import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.vaadin.lineawesome.LineAwesomeIconUrl;
+
+@PageTitle("My View")
+@Route("")
+@Menu(order = 0, icon = LineAwesomeIconUrl.PENCIL_RULER_SOLID)
+@Uses(Icon.class)
+public class MyViewView extends Composite<VerticalLayout> {
+
+    public MyViewView() {
+        HorizontalLayout layoutRow = new HorizontalLayout();
+        VerticalLayout layoutColumn3 = new VerticalLayout();
+        VerticalLayout layoutColumn2 = new VerticalLayout();
+        Grid basicGrid = new Grid(SamplePerson.class);
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+        layoutRow.addClassName(Gap.MEDIUM);
+        layoutRow.setWidth("100%");
+        layoutRow.getStyle().set("flex-grow", "1");
+        layoutColumn3.getStyle().set("flex-grow", "1");
+        layoutColumn2.setWidth("100%");
+        layoutColumn2.getStyle().set("flex-grow", "1");
+        basicGrid.setWidth("100%");
+        basicGrid.getStyle().set("flex-grow", "0");
+        setGridSampleData(basicGrid);
+        getContent().add(layoutRow);
+        layoutRow.add(layoutColumn3);
+        layoutRow.add(layoutColumn2);
+        layoutColumn2.add(basicGrid);
+    }
+
+    private void setGridSampleData(Grid grid) {
+        grid.setItems(query -> samplePersonService.list(
+                PageRequest.of(query.getPage(), query.getPageSize(), VaadinSpringDataHelpers.toSpringDataSort(query)))
+                .stream());
+    }
+
+    @Autowired()
+    private SamplePersonService samplePersonService;
+}
